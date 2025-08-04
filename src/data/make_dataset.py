be119ae796063829c760df07371c8f01e461d319
David Zhao <115097081+Kamisatodavid@users.noreply.github.com>
2025-08-04 12:12:22 -0400

    cleaning data: finding the optiamal time to merge

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -60,23 +60,77 @@ for f in files:
         gyr_set += 1
         gyr_df = pd.concat([gyr_df, df])
 
-
-
-
 # --------------------------------------------------------------
 # Working with datetimes
 # --------------------------------------------------------------
+acc_df.info()
 
+pd.to_datetime(acc_df["epoch (ms)"], unit='ms')
 
+acc_df.index = pd.to_datetime(acc_df["epoch (ms)"], unit='ms')
+gyr_df.index = pd.to_datetime(gyr_df["epoch (ms)"], unit='ms')
+
+del acc_df["epoch (ms)"]
+del acc_df["time (01:00)"]
+del acc_df["elapsed (s)"]
+
+del gyr_df["epoch (ms)"]
+del gyr_df["time (01:00)"]
+del gyr_df["elapsed (s)"]
 # --------------------------------------------------------------
 # Turn into function
 # --------------------------------------------------------------
+files = glob("../../data/raw/MetaMotion/*.csv")
+def read_data_from_files(files):
+    acc_df = pd.DataFrame()
+    gyr_df = pd.DataFrame()
+    
+    acc_set = 1
+    gyr_set = 1
+
+    for f in files:
+        participant = f.split("-")[0].replace(data_path, "")
+        label = f.split("-")[1]
+        category = f.split("-")[2].rstrip("123").rstrip("_MetaWear_2019")
+
+        df = pd.read_csv(f)
+
+        df["participant"] = participant
+        df["label"] = label
+        df["category"] = category
+
+        if "Accelerometer" in f:
+            df["set"] = acc_set
+            acc_set += 1
+            acc_df = pd.concat([acc_df, df])
+
+        if "Gyroscope" in f:
+            df["set"] = gyr_set
+            gyr_set += 1
+            gyr_df = pd.concat([gyr_df, df])
+
+    acc_df.index = pd.to_datetime(acc_df["epoch (ms)"], unit='ms')
+    gyr_df.index = pd.to_datetime(gyr_df["epoch (ms)"], unit='ms')
+
+    del acc_df["epoch (ms)"]
+    del acc_df["time (01:00)"]
+    del acc_df["elapsed (s)"]
+
+    del gyr_df["epoch (ms)"]
+    del gyr_df["time (01:00)"]
+    del gyr_df["elapsed (s)"]
+    
+    return acc_df, gyr_df
+
+acc_df,  = read_data_from_files(files)
 
 
 # --------------------------------------------------------------
 # Merging datasets
 # --------------------------------------------------------------
+data_merged = pd.concat([acc_df.iloc[:,:3], gyr_df], axis=1)
 
+data_merged.columns = ["acc_x", "acc_y", "acc_z", "gyr_x", "gyr_y", "gyr_z", "participant", "label", "category", "set"]
 
 # --------------------------------------------------------------
 # Resample data (frequency conversion)
@@ -85,7 +139,7 @@ for f in files:
 # Accelerometer:    12.500HZ
 # Gyroscope:        25.000Hz
 
-
+data_merged.iloc[:100].select_dtypes(include="number").resample(rule = "200ms").mean()
 # --------------------------------------------------------------
 # Export dataset
 # --------------------------------------------------------------
