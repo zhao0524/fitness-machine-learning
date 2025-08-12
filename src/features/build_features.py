4e357e5fd0d89ffb9fdc6daf2ec7ae89fb332ccc
David Zhao <115097081+Kamisatodavid@users.noreply.github.com>
2025-08-12 10:20:40 -0400

    frequency feature

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from DataTransformation import LowPassFilter, PrincipalComponentAnalysis
 from TemporalAbstraction import NumericalAbstraction
-
+from FrequencyAbstraction import FourierTransformation
 
 # --------------------------------------------------------------
 # Load data
@@ -153,12 +153,42 @@ subset[["gyr_y", "gyr_y_temp_mean_ws_5", "gyr_y_temp_std_ws_5"]].plot()
 # --------------------------------------------------------------
 # Frequency features
 # --------------------------------------------------------------
+df_freq = df_temporal.copy().reset_index()
+FreqAbs = FourierTransformation()
+
+fs = int(1000/200)
+ws = int(2800 / 200)
+
+
+df_freq = FreqAbs.abstract_frequency(df_freq, ["acc_y"], ws, fs)
+
+subset = df_freq[df_freq["set"] == 25]
+subset[["acc_y"]].plot()
+subset[[
+    "acc_y_max_freq",
+    "acc_y_freq_weighted",
+    "acc_y_pse",
+    "acc_y_freq_1.429_Hz_ws_14", 
+    "acc_y_freq_2.5_Hz_ws_14"
+]].plot()
+
+df_freq_list = []
+for s in df_freq["set"].unique():
+    print(f"Applying fourier transformation for set: {s}")
+    subset = df_freq[df_freq["set"] == s].reset_index(drop=True).copy()
+    subset = FreqAbs.abstract_frequency(subset, predictor_columns, ws, fs)
+    df_freq_list.append(subset)
+
+df_freq = pd.concat(df_freq_list).set_index("epoch (ms)", drop = True)
 
 
 # --------------------------------------------------------------
 # Dealing with overlapping windows
 # --------------------------------------------------------------
 
+df_freq.dropna()
+
+df_freq.iloc[::2]
 
 # --------------------------------------------------------------
 # Clustering
