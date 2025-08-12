f729c82c701bc70ec3e526a28398abcc97992ff6
David Zhao <115097081+Kamisatodavid@users.noreply.github.com>
2025-08-12 10:51:56 -0400

    exported the data features

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from DataTransformation import LowPassFilter, PrincipalComponentAnalysis
 from TemporalAbstraction import NumericalAbstraction
 from FrequencyAbstraction import FourierTransformation
+from sklearn.cluster import KMeans
 
 # --------------------------------------------------------------
 # Load data
@@ -186,15 +187,61 @@ df_freq = pd.concat(df_freq_list).set_index("epoch (ms)", drop = True)
 # Dealing with overlapping windows
 # --------------------------------------------------------------
 
-df_freq.dropna()
+df_freq = df_freq.dropna()
 
-df_freq.iloc[::2]
+df_freq = df_freq.iloc[::2]
+
 
 # --------------------------------------------------------------
 # Clustering
 # --------------------------------------------------------------
+df_cluster = df_freq.copy()
 
+cluster_columns = ["acc_x", "acc_y", "acc_z"]
+k_value = range(2, 10)
+inertias = []
 
+for k in k_value:
+    subset = df_cluster[cluster_columns]
+    kmeans = KMeans(n_clusters=k, n_init=20, random_state=0)
+    cluster_labels = kmeans.fit_predict(subset)
+    inertias.append(kmeans.inertia_)
+    
+plt.figure(figsize=(10, 10))
+plt.plot(k_value, inertias)
+plt.xlabel('Number of clusters (k)')
+plt.ylabel('sum of square distance')
+plt.show()
+
+# 5 looks like the optimal amount
+
+kmeans = KMeans(n_clusters=5, n_init=20, random_state=0)
+subset = df_cluster[cluster_columns]
+df_cluster["cluster"] = kmeans.fit_predict(subset)
+
+fig = plt.figure(figsize=(15,15))
+ax = fig.add_subplot(projection='3d')
+for c in df_cluster["cluster"].unique():
+    subset = df_cluster[df_cluster["cluster"] == c]
+    ax.scatter(subset["acc_x"], subset["acc_y"], subset["acc_z"], label=f"Cluster {c}")
+ax.set_xlabel('x_axis')
+ax.set_ylabel('y_axis')
+ax.set_zlabel('z_axis')
+ax.legend()
+plt.show()
+
+fig = plt.figure(figsize=(15,15))
+ax = fig.add_subplot(projection='3d')
+for c in df_cluster["label"].unique():
+    subset = df_cluster[df_cluster["label"] == c]
+    ax.scatter(subset["acc_x"], subset["acc_y"], subset["acc_z"], label=f"Cluster {c}")
+ax.set_xlabel('x_axis')
+ax.set_ylabel('y_axis')
+ax.set_zlabel('z_axis')
+ax.legend()
+plt.show()
 # --------------------------------------------------------------
 # Export dataset
 # --------------------------------------------------------------
+
+df_cluster.to_pickle("../../data/interim/03_data_features.pkl")
