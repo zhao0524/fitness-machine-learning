47e3528ea2997ab35f0005fca618c5067bac2dea
David Zhao <115097081+Kamisatodavid@users.noreply.github.com>
2025-08-14 12:44:08 -0400

    check for final result

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -211,12 +211,54 @@ for i, f in zip(range(len(possible_feature_sets)), feature_names):
 # --------------------------------------------------------------
 score_df.sort_values(by="accuracy", ascending=False)
 
+plt.figure(figsize=(10, 10))
+sns.barplot(x="model", y="accuracy", hue="feature_set", data=score_df)
+plt.xlabel("Model")
+plt.ylabel("Accuracy")
+plt.ylim(0.7, 1)
+plt.legend(loc="lower right")
+plt.show()
 
 # --------------------------------------------------------------
 # Select best model and evaluate results
 # --------------------------------------------------------------
 
+(
+class_train_y,
+class_test_y,
+class_train_prob_y,
+class_test_prob_y,
+) = learner.random_forest(
+X_train[feature_set_4], Y_train, X_test[feature_set_4], gridsearch=True
+)
 
+accuracy = accuracy_score(Y_test, class_test_y)
+
+classes = class_test_prob_y.columns
+cm = confusion_matrix(Y_test, class_test_y, labels=classes)
+
+# create confusion matrix for cm
+plt.figure(figsize=(10, 10))
+plt.imshow(cm, interpolation="nearest", cmap=plt.cm.Blues)
+plt.title("Confusion matrix")
+plt.colorbar()
+tick_marks = np.arange(len(classes))
+plt.xticks(tick_marks, classes, rotation=45)
+plt.yticks(tick_marks, classes)
+
+thresh = cm.max() / 2.0
+for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+    plt.text(
+        j,
+        i,
+        format(cm[i, j]),
+        horizontalalignment="center",
+        color="white" if cm[i, j] > thresh else "black",
+    )
+plt.ylabel("True label")
+plt.xlabel("Predicted label")
+plt.grid(False)
+plt.show()
 # --------------------------------------------------------------
 # Select train and test data based on participant
 # --------------------------------------------------------------
